55ff727993555206939bd7ef3d5cc4ab88628321
JOÃO PEDRO SILVA <joao_silva26@estudante.sesisenai.org.br>
2024-10-07 11:33:51 -0300

    Commit camera

--- CalculadoraExpo/app/camera/index.jsx
+++ CalculadoraExpo/app/camera/index.jsx
@@ -1,90 +1,86 @@
-import { useState, useRef } from "react"
-import { View, StyleSheet, Text, Image, Button, TouchableOpacity } from 'react-native'
-import { CameraView, useCameraPermissions, CameraType } from 'expo-camera'
+import { useState, useRef } from "react";
+import { View, StyleSheet, Text, Image, Button, SafeAreaView } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function Camero() {
-    const [permissao, pedirPermissao] = useCameraPermissions()
-    const [facing, setFacing] = useState('back')
-    const [foto, setFoto] = useState(null)
-    const cameraRef = useRef(null)
-
+    const [permissao, pedirPermissao] = useCameraPermissions();
+    const [facing, setFacing] = useState('back');
+    const [foto, setFoto] = useState(null);
+    const cameraRef = useRef(null);
 
     function trocaCamera() {
-        setFacing(current => (current === 'back' ? 'front' : 'back'))
+        setFacing(ladoCamera => (ladoCamera === 'back' ? 'front' : 'back'));
     }
 
     if (!permissao) {
-        return (
-            <View></View>
-        )
+        return <View />;
     }
 
     if (!permissao.granted) {
         return (
             <View style={style.container}>
-                <Text style={style.textopermissao}>Você Precissa Permitir a Camera</Text>
+                <Text style={style.textopermissao}>Você Precisa Permitir a Câmera</Text>
                 <Button style={style.botao} title="Permitir" onPress={pedirPermissao} />
             </View>
-        )
-    }
-    const tirarFoto = async () => {
-        const foto = await cameraRef.current?.takePictureAsync({
-            quality: 0.5,
-            base64: true
-        })
-        setFoto(foto)
-        console.log(foto)
-        
+        );
     }
 
+    const tirarFoto = async () => {
+        const fotoBase64 = await cameraRef.current?.takePictureAsync({
+            quality: 1,
+            base64: true
+        });
+        setFoto(fotoBase64);
+        console.log(fotoBase64);
+    };
+
+    const salvarFoto = async () => {
+        MediaLibrary,MediaLibrary.saveToLibraryAsync(foto.uri)
+        setFoto(null)
+    }
 
     return (
-        <View style={style.container}>
-            <CameraView facing={facing} style={style.camera} ref={cameraRef}>
-                <View>
-                    <Button style={style.botaoB} title="OI" onPress={tirarFoto}/>
-                    <Button style={style.botaoB} title="TrocarCamera" onPress={trocaCamera}/>
-                    
+        <SafeAreaView style={style.container}>
+            {foto ? (
+                <View style={style.camera}>
+                    <Image style={style.image} source={{uri: foto.uri}} resizeMode="contain" />
+                    <Button title="Salvar" onPress={salvarFoto} />
+                    <Button title="Descartar" onPress={() => setFoto(null)} />
                 </View>
-            </CameraView>
-        </View>
-
-    )
+            ) : (
+                <CameraView facing={facing} style={style.camera} ref={cameraRef}>
+                    <SafeAreaView>
+                        <Button title="Tirar Foto" onPress={tirarFoto} />
+                        <Button title="Trocar Câmera" onPress={trocaCamera} />
+                    </SafeAreaView>
+                </CameraView>
+            )}
+        </SafeAreaView>
+    );
 }
 
 const style = StyleSheet.create({
     container: {
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        height: '100%',
     },
     botao: {
         flex: 1,
-        alignSelf: 'flex-end',
+        alignSelf: 'center',
         alignItems: 'center',
-
     },
     textopermissao: {
         textAlign: 'center'
     },
     camera: {
         width: '100%',
-        height: '100%'
+        height: '100%',
+        justifyContent: 'flex-end'
     },
-    text: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: 'white',
+    image: {
+        height: '100%',
+        width: '100%'
     },
-    botaoCamera: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: 'transparent',
-        margin: 64,
-    },
-    botaoB: {
-        
-    }
-
-
-})
+});
