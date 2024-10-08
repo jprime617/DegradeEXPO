46000241d728f7c9c98cf2b3e4b0afc843d2512e
JOÃO PEDRO SILVA <joao_silva26@estudante.sesisenai.org.br>
2024-10-08 12:10:28 -0300

    commit camera2

--- CalculadoraExpo/app/camera/index.jsx
+++ CalculadoraExpo/app/camera/index.jsx
@@ -1,8 +1,12 @@
 import { useState, useRef } from "react";
-import { View, StyleSheet, Text, Image, Button, SafeAreaView } from 'react-native';
+import { View, StyleSheet, Text, Image, Button, SafeAreaView, TouchableOpacity, Pressable } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import * as Linking from 'expo-linking';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Camero() {
     const [permissao, pedirPermissao] = useCameraPermissions();
@@ -10,6 +14,7 @@ export default function Camero() {
     const [foto, setFoto] = useState(null);
     const cameraRef = useRef(null);
     const [linkEscaneado, setLinkescaneado] = useState(false)
+    const [fotoTirada, setFotoTirada] = useState(false)
 
     function trocaCamera() {
         setFacing(ladoCamera => (ladoCamera === 'back' ? 'front' : 'back'));
@@ -29,16 +34,17 @@ export default function Camero() {
     }
 
     const tirarFoto = async () => {
-        const fotoBase64 = await cameraRef.current?.takePictureAsync({
-            quality: 1,
-            base64: true
-        });
-        setFoto(fotoBase64);
-        console.log(fotoBase64);
+            setFotoTirada(true)
+            const fotoBase64 = await cameraRef.current?.takePictureAsync({
+                quality: 1,
+                base64: true
+            });
+            setFoto(fotoBase64);
+            setTimeout(() => setFotoTirada(false), 1000)
     };
 
     const salvarFoto = async () => {
-        MediaLibrary,MediaLibrary.saveToLibraryAsync(foto.uri)
+        MediaLibrary, MediaLibrary.saveToLibraryAsync(foto.uri)
         setFoto(null)
     }
 
@@ -46,8 +52,8 @@ export default function Camero() {
         if (!linkEscaneado) {
             Linking.openURL(codeL.data)
             setLinkescaneado(true)
-            setTimeout(() => setLinkescaneado(false),5000)
-            
+            setTimeout(() => setLinkescaneado(false), 5000)
+
         }
     }
 
@@ -55,17 +61,20 @@ export default function Camero() {
         <SafeAreaView style={style.container}>
             {foto ? (
                 <View style={style.camera}>
-                    <Image style={style.image} source={{uri: foto.uri}} resizeMode="contain" />
-                    <Button title="Salvar" onPress={salvarFoto} />
-                    <Button title="Descartar" onPress={() => setFoto(null)} />
+                    <Image style={style.image} source={{ uri: foto.uri }} resizeMode="contain" />
+                    <SafeAreaView style={style.buttonContainer}>
+                        <Pressable style={style.botaoA} onPress={salvarFoto}><MaterialIcons name="save-alt" size={60} color="black" /></Pressable>
+                        <Pressable style={style.botaoO} onPress={() => setFoto(null)}><FontAwesome name="remove" size={50} color="black" /></Pressable>
+                    </SafeAreaView>
                 </View>
             ) : (
                 <CameraView barcodeScannerSettings={{
                     barcodeTypes: ['qr']
                 }} onBarcodeScanned={(code) => LinkFoiEscaneado(code)} facing={facing} style={style.camera} ref={cameraRef}>
-                    <SafeAreaView>
-                        <Button title="Tirar Foto" onPress={tirarFoto} />
-                        <Button title="Trocar Câmera" onPress={trocaCamera} />
+                    <SafeAreaView style={style.buttonContainer}>
+                        <Pressable style={style.botaoS} onPress={() => (!fotoTirada) ? tirarFoto() : console.log('calma la paizao')}><Ionicons name="camera" size={60} color="black" /></Pressable>
+                        <Pressable style={style.botaoR} onPress={trocaCamera}><FontAwesome6 name="camera-rotate" size={40} color="black" /></Pressable>
+
                     </SafeAreaView>
                 </CameraView>
             )}
@@ -77,7 +86,7 @@ const style = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#f0f0f0', // Cor de fundo suave
+        backgroundColor: '#f0f0f0',
     },
     textopermissao: {
         textAlign: 'center',
@@ -87,24 +96,47 @@ const style = StyleSheet.create({
     },
     camera: {
         flex: 1,
-        justifyContent: 'flex-end',
+        justifyContent: 'center',
     },
     image: {
-        height: '70%', // Ajuste a altura da imagem
+        height: '70%',
         width: '100%',
-        borderRadius: 10, // Bordas arredondadas
-        marginBottom: 10, // Espaço abaixo da imagem
+        borderRadius: 10,
+        marginBottom: 10,
     },
     controls: {
         flexDirection: 'row',
         justifyContent: 'space-around',
         padding: 20,
-        backgroundColor: 'rgba(255, 255, 255, 0.8)', // Fundo semitransparente
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
     },
     buttonContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
         padding: 10,
         marginBottom: 20,
     },
+    botaoS: {
+        backgroundColor: 'none',
+        position: 'absolute',
+        top: 380
+    },
+    botaoR: {
+        backgroundColor: 'none',
+        position: 'absolute',
+        top: 393,
+        left: 50
+    },
+    botaoA: {
+        backgroundColor: 'none',
+        position: 'absolute',
+        top: 50,
+    },
+    botaoO: {
+        backgroundColor: 'none',
+        position: 'absolute',
+        top: 55,
+        left: 40
+    },
+
 });
