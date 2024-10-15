05fe85fcc0b2bde54f7403c6c07c7488a95835fe
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-10-15 11:46:30 -0300

    commit fazer em casa

--- CalculadoraExpo/app/memoria/index.jsx
+++ CalculadoraExpo/app/memoria/index.jsx
@@ -2,95 +2,92 @@ import { View, Text, Pressable, StyleSheet, TextInput, Button } from 'react-nati
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState, useEffect } from 'react';
 
-
-
-const saveData = async (save, array) => {
+const storeData = async (value) => {
     try {
-        const newData = { ...array, id: array.length + 1}
-        array.push(save)
-        array.push(newData)
-        
-        console.log(array)
-        
-    } catch (error) {
-        console.log(error)
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem('my-key', jsonValue);
+        console.log('Data saved');
+    } catch (e) {
+        console.log(e);
     }
-}
+};
 
 const getData = async () => {
-    try{
-        const value = await AsyncStorage.getItem('my-key')
-        console.log(value)
-        if (value !== null) {
-            console.log('tem coisa ai')
-        }
-    } catch(error){
-        console.log(error)
+    try {
+      const jsonValue = await AsyncStorage.getItem('my-key');
+      return jsonValue != null ? console.log(JSON.parse(jsonValue)) : null;
+    } catch (e) {
+      console.log(e);
     }
-}
-
-const clearData = async () => {
-    try{
-        await AsyncStorage.removeItem('my-key')
-    }catch(error){
-        console.log(error)
-    }
-}
+};
 
 export default function Memoria() {
-    const [info, setInfo] = useState('')
-    const [data, setData] = useState({id: 0, titulo: '', data: '', onde: '', desc: '', uri: ''})
-    const [storageData, setStorageData] = useState([])
-    const massa = []
+    const [formData, setFormData] = useState({
+        titulo: '',
+        ano: '',
+        onde: '',
+        desc: ''
+    });
 
+    // Função para atualizar o estado com base no campo que foi alterado
+    const handleInputChange = (name, value) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [name]: value
+        }));
+    };
 
     return (
         <View style={styles.container}>
-            <Text>Titulo</Text>
             <TextInput
+                placeholder='Título'
+                value={formData.titulo}
+                onChangeText={(value) => handleInputChange('titulo', value)}
                 style={styles.input}
-                placeholder="Titulo"
-                onChangeText={(text) => setData({...data, titulo: text})}
-                value={data.titulo}
             />
-            <Text>data</Text>
             <TextInput
+                placeholder='Ano'
+                value={formData.ano}
+                onChangeText={(value) => handleInputChange('ano', value)}
                 style={styles.input}
-                placeholder="data"
-                onChangeText={(text) => setData({...data, data: text})}
-                value={data.data}
             />
-            <Text>onde</Text>
             <TextInput
+                placeholder='Onde'
+                value={formData.onde}
+                onChangeText={(value) => handleInputChange('onde', value)}
                 style={styles.input}
-                placeholder="Onde"
-                onChangeText={(text) => setData({...data, onde: text})}
-                value={data.onde}
             />
-            <Text>desc</Text>
             <TextInput
+                placeholder='Descrição'
+                value={formData.desc}
+                onChangeText={(value) => handleInputChange('desc', value)}
                 style={styles.input}
-                placeholder="desc"
-                onChangeText={(text) => setData({...data, desc: text})}
-                value={data.desc}
             />
-            <Button title="Set" onPress={() => {saveData(data, massa);}}/>
-            <Button title="Get" onPress={getData}/>
-            <Button title='Clear' onPress={clearData}/>
-            <Button title='mostra data' onPress={() => console.log(data)}/>
+            <Button
+                title='Salvar'
+                onPress={() => storeData(formData)}
+            />
+            <Button
+                title='Receba'
+                onPress={() => getData()}
+            />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        padding: 20
     },
     input: {
         backgroundColor: 'gray',
         height: 50,
-        width: 'auto'
+        width: '100%',
+        marginVertical: 10,
+        padding: 10,
+        borderRadius: 5,
     }
-})
+});
